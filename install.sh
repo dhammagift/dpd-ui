3550e1bac09aca829204ce568fa3db989f1e7ae0
root <root@o.>
2026-05-01 14:11:43 +0500

    added install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
+.venv/bin/pip3 install uvicorn httpx fastapi jinja2
 
 
 #stsrt local db
-.venv/bin/uvicorn exporter.webapp.main:app --host 0.0.0.0 --port 8080
+.venv/bin/uvicorn exporter.webapp.main:app --host 0.0.0.0 --port 8880
 
 
