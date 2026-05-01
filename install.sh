ce098be1a67e85749ad920f9e892ed205c6b15e8
root <root@o.>
2026-05-01 14:23:25 +0500

    use requirements.txt in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-.venv/bin/pip3 install uvicorn httpx fastapi jinja2
 
+python3 -m venv .venv
 
+.venv/bin/pip3 install -r requirements.txt
+
+bash start.sh 
+
+exit 0
 #stsrt local db
 .venv/bin/uvicorn exporter.webapp.main:app --host 0.0.0.0 --port 8880
 
